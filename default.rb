19e3ece924f3fd2c484d0d1c19c7a8bfa87fb603
kucaahbe <kucaahbe@ukr.net>
2012-04-20 18:10:28 +0300

    assets logging,config files,comments

--- default.rb
+++ default.rb
@@ -3,19 +3,30 @@
 # haml
 # rspec
 # cucumber
-# gitignores
 
 remove_file 'vendor/plugins'
+create_file 'config/examples/.gitkeep'
+append_to_file '.gitignore' do
+  <<-IGN
+
+# ignore config files.
+/config/*.yml
+  IGN
+end
 
 # uncomment therubyracer:
 gsub_file 'Gemfile', "# gem 'therubyracer'", "gem 'therubyracer'"
-
 # remove unused comments in a Gemfile
 ['Bundle edge', 'gem', 'To use', 'Deploy', 'Use'].each do |regexp|
   gsub_file 'Gemfile', /^#\s#{regexp}.*\n?$/, ''
 end
-
+# add usable gems
 gem_group :development do
   gem "letter_opener"
   gem "thin"
+end
+
+# turn off assets logging
+environment(nil, :env => "development") do
+  "config.assets.logger = false"
 end
